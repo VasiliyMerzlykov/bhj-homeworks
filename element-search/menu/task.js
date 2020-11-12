77b567662584d4fdfa362091652b2851cf2c2230
Vasiliy Merzlyakov <V.Merov@yandex.ru>
2020-11-12 15:11:01 +0500

    element-search

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,6 +1,11 @@
-const click = document.getElementsByClassName("menu__link")
+const menu = Array.from(document.querySelectorAll(".menu__link"))
 
-for(let i = 0; i < click.length; i++){
-    const subMenus = click[i].getElementsByClassName("menu__sub");    
-    console.log(subMenus)
+for(let i = 0; i < menu.length; i++){
+    menu[i].onclick = () => {
+        let elem = menu[i].parentElement.querySelector('.menu_sub')
+        if(elem.classList.contains("menu_active")){
+            elem.classList.remove("menu_active")
+        } else elem.classList.add("menu_active")
+        return false;
+    };
 };
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,21 +1,24 @@
-const nextSlide = document.getElementsByClassName("slider__arrow slider__arrow_next")[0];
-const lastSlide = document.getElementsByClassName("slider__arrow slider__arrow_prev")[0];
-let listElement = document.getElementsByClassName("slider__item");
-let arr = Array.from(listElement);
+let nextSlide = document.getElementsByClassName("slider__arrow_next")[0]
+let lastSlide = document.getElementsByClassName("slider__arrow_prev")[0]
+let slide = Array.from(document.getElementsByClassName("slider__item"))
 
-function getStr(arrs){
-    return arrs.classList[1] === "slider__item_active"
-};
+nextSlide.onclick = () => {
+    let indexSlide = slide.findIndex(item =>item.className === "slider__item slider__item_active")
+    if(indexSlide == slide.length - 1) {
+        slide[0].classList.add("slider__item_active")
+    };
+    slide[indexSlide].classList.remove("slider__item_active")
+    slide[indexSlide + 1].classList.add("slider__item_active")   
+}
 
-nextSlide.onclick = function (){
-    let a = arr.findIndex(getStr);   
-    arr[a].classList.remove("slider__item_active");    
-    (a == arr.length - 1) ?arr[0].classList.add("slider__item_active"):arr[a+1].classList.add("slider__item_active");
-};
+lastSlide.onclick = () => {
+    let indexSlide = slide.findIndex(item =>item.className === "slider__item slider__item_active")
+    if( indexSlide == 0){
+        slide[slide.length - 1].classList.add("slider__item_active")
+    }
+    slide[indexSlide].classList.remove("slider__item_active");
+    slide[indexSlide-1].classList.add("slider__item_active");
+}
 
-lastSlide.onclick = function (){
-    let a = arr.findIndex(getStr);   
-    arr[a].classList.remove("slider__item_active");    
-    (a == 0)? arr[arr.length - 1].classList.add("slider__item_active"):arr[a-1].classList.add("slider__item_active");     
-};
+
 
