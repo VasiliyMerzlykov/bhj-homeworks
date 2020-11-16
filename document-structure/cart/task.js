d5921eea21ceebefab86d688ff2aa12526a33c81
Vasiliy Merzlyakov <V.Merov@yandex.ru>
2020-11-16 17:19:00 +0500

    new commit

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,7 +1,6 @@
 const product = document.querySelectorAll('.product');
 const cartProducts = document.querySelector('.cart__products');
 
-
 [...product].map(item => {
     item.addEventListener('click', e =>{
         const value = item.querySelector('.product__quantity-value')
@@ -33,59 +32,4 @@ const cartProducts = document.querySelector('.cart__products');
         };
     })
 })
-
-
-
-// const productQuantityControls = Array.from(document.querySelectorAll('.product__quantity-controls'));
-// const product = Array.from(document.querySelectorAll('.product'));
-// const cartProducts = document.querySelector('.cart__products');
-// const cart = document.getElementsByClassName("cart__title");
-
-
-// for (let i = 0; i < productQuantityControls.length; i++) {
-//     console.log(document.querySelector("cart__product"))
-//     productQuantityControls[i].addEventListener('click', (event) => {
-//         let value = productQuantityControls[i].querySelector('.product__quantity-value');
-
-//         if (event.target.matches('.product__quantity-control_dec')) {
-//             if (value.innerText == 1) {
-//                 return;
-//             }
-//             --value.innerText;            
-//         } else if (event.target.matches('.product__quantity-control_inc')) {
-//             ++value.innerText;          
-//         }
-//         return value.innerText;
-//     })
-// }
-
-// for (let i = 0; i < product.length; i++) {
-//     product[i].addEventListener('click', (event) => {
-//         const cartProduct = Array.from(document.querySelectorAll('.cart__product'));      
-
-//         const id = product[i].dataset.id;
-//         const value = Number(product[i].querySelector('.product__quantity-value').innerText);
-//         const img = product[i].querySelector('.product__image').getAttribute('src');
-        
-        // const elementParent = cartProduct.find((elem) => {
-        //     return elem.dataset.id === id;
-        //     // return elem.dataset.id === event.target.closest('.product').dataset.id;
-        // })            
-            
-        // if (elementParent) {
-        //     if (event.target.matches('.product__add')) {
-        //         let valueAdd = Number(elementParent.querySelector('.cart__product-count').innerText);
-        //         elementParent.querySelector('.cart__product-count').innerText = valueAdd + value;
-        //     }            
-//         } else {
-//             if (event.target.matches('.product__add')) {
-//                 cartProducts.insertAdjacentHTML('beforeEnd', `
-//                 <div class="cart__product" data-id="${id}">
-//                     <img class="cart__product-image" src="${img}">
-//                     <div class="cart__product-count">${value}</div>
-//                 </div>`);
-//             }
-//         }
-//     })
-// }
 
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,9 +2,7 @@ const arr = document.querySelectorAll('.has-tooltip');
 
 [...arr].map(item => {
     item.insertAdjacentHTML('afterend', '<div class="tooltip" style="left: 0; top: 0"></div>');
-    item.onclick = function() {    
-        const tooltip = document.getElementsByClassName('tooltip_active');
-        [...tooltip].map(item => item.classList.remove('tooltip_active'))
+    item.onclick = function() {            
         item.nextElementSibling.classList.toggle('tooltip_active');
         item.nextElementSibling.textContent = item.title;
         item.nextElementSibling.style.top = `${item.getBoundingClientRect().bottom}px`;
